adfc4670df0b0e4bd7bfdfd4d5fc49f9c971657c
juancv3d <juancvillarios@hotmail.com>
2021-09-24 05:04:11 -0500

    Function to get the form info and display it

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
 app = Flask(__name__)
 
+logs = []
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template('home.html')
+    if request.method == "POST":
+        log = request.form.get('log')
+        date = datetime.datetime.today().strftime('%Y-%m-%d')
+        date_formated = datetime.datetime.strptime(
+            date, '%Y-%m-%d').strftime('%b %d')
+        if len(log) > 0:
+            logs.append((date, log, date_formated))
+    return render_template('home.html', logs=logs)

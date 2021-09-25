807fa5d3c81b005d91b816a3bdc4f824ad465941
juancv3d <juancvillarios@hotmail.com>
2021-09-25 03:26:55 -0500

    Managing the database with mongodb

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import datetime
+from pymongo import MongoClient
+import os
 
-app = Flask(__name__)
 
-logs = []
+def create_app():
+    # Create and configure the app
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret'
+    # Set up mongo connection
+    client = MongoClient(os.environ.get('MONGO_CLIENT'))
+    app.db = client.microblog
 
+    @app.route('/', methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            log = request.form.get('log')
+            date = datetime.datetime.today().strftime('%Y-%m-%d')
+            date_formated = datetime.datetime.strptime(
+                date, '%Y-%m-%d').strftime('%b %d')
 
-@app.route('/', methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        log = request.form.get('log')
-        date = datetime.datetime.today().strftime('%Y-%m-%d')
-        date_formated = datetime.datetime.strptime(
-            date, '%Y-%m-%d').strftime('%b %d')
-        if len(log) > 0:
-            logs.append((date, log, date_formated))
-    return render_template('home.html', logs=logs)
+            if len(log) > 0:
+                app.db.logs.insert_one({
+                    'log': log,
+                    'date_formated': date_formated,
+                    'date': date
+                })
+                flash("Log added", "success")
+            else:
+                flash("Log can't be empty", "error")
+
+        return render_template('home.html', logs=app.db.logs.find().sort('_id', -1).limit(10))
+
+    return app
